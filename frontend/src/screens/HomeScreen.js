33e15523745440810c1dced2805c8b0431f6a7e1
PRATIKRAMANI2411 <pratikramani22@gmail.com>
2023-08-08 23:03:41 +0530

    usereducer-for-data-fetch-products-data

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { Link } from "react-router-dom";
-// import data from '../data';
 import axios from "axios";
 
+const reduser = (state, action) => {
+    // console.log(action.payload)
+    // console.log(state, action)
+    switch (action.type) {
+        case 'FETCH_REQUEST':
+            return { ...state, logging: true };
+        case 'FETCH_SUCCESS':
+            return { ...state, products: action.payload, loading: false };
+        case 'FETCH_FAIL':
+            return { ...state, error: action.payload, loading: false }
+        default:
+            return state;
+    }
+}
+
 export default function HomeScreen() {
-    const [products, Setproducts] = useState([]);
+
+    const [{ loading, error, products }, dispatch] = useReducer(reduser, {
+        product: [],
+        loading: true,
+        error: '',
+    });
+    // console.log(products)
+
+    // const [products, Setproducts] = useState([]);
     useEffect(() => {
         const fatchData = async () => {
-            const results = await axios.get('/api/products');
-            Setproducts(results.data)
+            dispatch({ type: 'FETCH_REQUEST' });
+            try {
+                const results = await axios.get('/api/products');
+                dispatch({ type: 'FETCH_SUCCESS', payload: results.data });
+            } catch (err) {
+                dispatch({ type: 'FETCH_FAIL', payload: err.message });
+            }
+            // Setproducts(results.data)
         }
         fatchData()
     }, []);
@@ -17,20 +45,23 @@ export default function HomeScreen() {
             <h1>Featured Product</h1>
             <div className="products">
                 {
-                    products.map((product) => (
-                        <div className="product" key={product.slug} >
-                            <Link to={`/product/${product.slug}`}>
-                                <img src={product.image} alt={product.name} />
-                            </Link>
-                            <div className="product-info">
-                                <Link to={`/product/${product.slug}`}>
-                                    <p>{product.name}</p>
-                                </Link>
-                                <p><strong>${product.price}</strong></p>
-                                <button>Add to Cart</button>
-                            </div>
-                        </div>
-                    ))
+                    loading ? <div>loading....</div>
+                        : error ? <div>{error}</div>
+                            :
+                            products.map((product) => (
+                                <div className="product" key={product.slug} >
+                                    <Link to={`/product/${product.slug}`}>
+                                        <img src={product.image} alt={product.name} />
+                                    </Link>
+                                    <div className="product-info">
+                                        <Link to={`/product/${product.slug}`}>
+                                            <p>{product.name}</p>
+                                        </Link>
+                                        <p><strong>${product.price}</strong></p>
+                                        <button>Add to Cart</button>
+                                    </div>
+                                </div>
+                            ))
                 }
             </div>
         </div>
