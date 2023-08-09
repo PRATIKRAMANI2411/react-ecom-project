c1ff5855931d07a3aef7c6f56e5d74528339940b
PRATIKRAMANI2411 <pratikramani22@gmail.com>
2023-08-09 08:33:56 +0530

    import-react-boostrap-and-added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,36 @@
 import './App.css';
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div>
-        <header>
-          <Link to="/">E-comm</Link>
-        </header>
-        <main>
-        <Routes>
-          <Route path="/product/:slug" element={<ProductScreen/>} />
-          <Route path="/" element={<HomeScreen/>} />
-        </Routes>
-        </main>
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="d-flex flex-column site-container">
+                <Navbar bg="dark" data-bs-theme="dark">
+                    <Container>
+                        <LinkContainer to="/">
+                            <Navbar.Brand>E-comm</Navbar.Brand>
+                        </LinkContainer>
+                    </Container>
+                </Navbar>
+                <main>
+                    <Container>
+                        <Routes>
+                            <Route path="/product/:slug" element={<ProductScreen />} />
+                            <Route path="/" element={<HomeScreen />} />
+                        </Routes>
+                    </Container>
+                </main>
+                <footer className="text-center">
+                    <div>All rights reserved</div>
+                </footer>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
