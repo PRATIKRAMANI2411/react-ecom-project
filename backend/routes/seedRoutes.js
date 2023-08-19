b3f9633792d904a1e37d19e4dc2de09f94c5374a
PRATIKRAMANI2411 <pratikramani22@gmail.com>
2023-08-19 12:05:09 +0530

    creat-sample-user

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import Product from "../models/productModel.js";
 import data from "../data.js";
+import User from "../models/userModel.js";
 const seedRouter = express.Router();
 
 seedRouter.get('/', async (req, resp) => {
     // await Product.remove({});
-    const creactProduct = await Product.insertMany(data.products);
-    resp.send({creactProduct})
+    // const creactProduct = await Product.insertMany(data.products);
+
+    // await User.remove({});
+    const creactUser = await User.insertMany(data.users);
+    resp.send({ creactUser })
 })
 export default seedRouter;
