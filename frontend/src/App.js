e398f6314ce34097dd3fae78d8b6061a3c5bc51b
PRATIKRAMANI2411 <pratikramani22@gmail.com>
2023-08-27 13:52:19 +0530

    solve-bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -47,7 +47,7 @@ function App() {
                         <Navbar.Collapse id='basic-navbar-nav'>
                             <Nav className="me-auto  w-100  justify-content-end">
                                 <Link to="/cart" className="nav-link">
-                                    cart
+                                    Cart
                                     {cart.cartItems.length > 0 && (<Badge pill bg="danger">{cart.cartItems.reduce((a, c) => a + c.quantity, 0)}</Badge>)}
                                 </Link>
                                 {userInfo ?
